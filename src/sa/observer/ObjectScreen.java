630f0db98e3657f4f38c4f7a101b8f6a270baba1
Ivan <elreizerwtf@gmail.com>
2024-11-04 12:44:18 -0300

    Interfaz y objetos auxiliares

---
+++ src/sa/observer/ObjectScreen.java
@@ -0,0 +1,13 @@
+package sa.observer;
+
+import sa.observer.interfaces.PopUpWindow;
+
+public class ObjectScreen implements PopUpWindow {
+
+	@Override
+	public void popUp(String message, String color, int fontSize) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/sa/observer/interfaces/INotifyObserver.java
@@ -0,0 +1,7 @@
+package sa.observer.interfaces;
+
+import sa.booking.Booking;
+
+public interface INotifyObserver {
+	public void update(Booking booking);
+}

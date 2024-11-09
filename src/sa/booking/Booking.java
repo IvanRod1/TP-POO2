9046b82151d7367f641fed9322b2702d8644384f
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-08 22:36:34 -0300

    Updated BookingTest.java and Booking.java

--- src/sa/booking/Booking.java
+++ src/sa/booking/Booking.java
@@ -1,31 +1,38 @@
 package sa.booking;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import sa.payments.PaymentMethod;
 import sa.policies.ICancellationPolicy;
 import sa.properties.Property;
+import sa.users.Tenant;
 
 public class Booking {
 
-	private IReserveState	state;
-	private ICancellationPolicy	policy;
+	private IReserveState			state;
+	private ICancellationPolicy		policy;
+	private Pricer					pricer;
+	private List<PaymentMethod>		paymentMethods;
 
 	public Booking(Property property, LocalDate checkIn, LocalDate checkOut, List<PaymentMethod> paymentMethods,
-			double pricePerDayWeekday, double pricePerDayHighSeason, double pricePerDayLongWeekend) {
+			double pricePerDayWeekday, List<Period> periods) {
 		// TODO Auto-generated constructor stub
-		this.state = (IReserveState) new ReserveAvailable();
+		this.state 					= (IReserveState) new ReserveAvailable();
+		this.pricer		 			= new Pricer(pricePerDayWeekday, periods);
 	}
 
 	// Para hacer DOC del state available
-	public Booking(ReserveAvailable stateAvailable, Property property, LocalDate checkIn, LocalDate checkOut,
-			List<PaymentMethod> paymentMethods, double pricePerDayWeekday, double pricePerDayHighSeason,
-			double pricePerDayLongWeekend) {
+	public Booking(ReserveAvailable stateAvailable, Pricer pricer, Property property, LocalDate checkIn, LocalDate checkOut,
+			List<PaymentMethod> paymentMethods, double pricePerDayWeekday, List<Period> periods) {
 		// TODO Auto-generated constructor stub
 		this.state = (IReserveState) stateAvailable;
-		
+		this.pricer = pricer;
+		this.pricer.setBasePrice(pricePerDayWeekday);
+		periods.stream().forEach(p -> this.pricer.addSpecialPeriod(p));
 	}
+
 
 	public IReserveState getState() {
 		// TODO Auto-generated method stub
@@ -52,4 +59,23 @@ public class Booking {
 		this.policy.activate();
 	}
 
+	public double price(LocalDate date) {
+		// TODO Auto-generated method stub
+		return this.pricer.price(date);
+	}
+
+	public double priceBetween(LocalDate startDate, LocalDate endDate) {
+		// TODO Auto-generated method stub
+		return this.pricer.priceBetween(startDate, endDate);
+	}
+
+	public void cancelReserve() {
+		// TODO Auto-generated method stub
+		this.state.cancelReserve();
+	}
+
+	public void reserve(Tenant t) {
+		// TODO Auto-generated method stub
+		this.state.requestReserve(t);		
+	}
 }
--- tests/sa/booking/BookingTest.java
+++ tests/sa/booking/BookingTest.java
@@ -1,5 +1,7 @@
 package sa.booking;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,6 +28,7 @@ import sa.users.Tenant;
 public class BookingTest {
 
 	private Booking				booking;
+	private Booking				bookingReal;
 	private ReserveAvailable	stateAvailable;
 	private ReserveApproved		stateApproved;
 	private ReserveCompleted	stateCompleted;
@@ -33,12 +36,17 @@ public class BookingTest {
 	private List<Tenant> 			tenants 	  	= new ArrayList<Tenant>();
 	private List<PaymentMethod> 	paymentMethods 	= new ArrayList<PaymentMethod>();
 	private List<INotifyObserver> 	subscribers	  	= new ArrayList<INotifyObserver>();
+	private List<Period> 			periods	  		= new ArrayList<Period>();
 
 	private INotifyObserver		subscriber1;
 	private INotifyObserver		subscriber2;
 
 	private CostFree			policy;
 	private Property			property;
+	private Pricer				pricer;
+	private Period				period1;
+	private Period				period2;
+	private Period				period3;
 	private Owner				owner;
 	private Tenant				tenant1;
 	private Tenant				tenant2;
@@ -65,6 +73,10 @@ public class BookingTest {
 
 		this.policy				= spy(CostFree.class);
 		this.property			= mock(Property.class);
+		this.pricer				= mock(Pricer.class);
+		this.period1			= mock(Period.class);
+		this.period2			= mock(Period.class);
+		this.period3			= mock(Period.class);
 		this.owner				= mock(Owner.class);
 		this.tenant1 	  		= mock(Tenant.class);
 		this.tenant2 	  		= mock(Tenant.class);
@@ -73,6 +85,9 @@ public class BookingTest {
 		this.subscriber1	  	= mock(INotifyObserver.class);
 		this.subscriber2	  	= mock(INotifyObserver.class);
 
+		this.periods.add(period1);
+		this.periods.add(period2);
+		this.periods.add(period3);
 		this.tenants.add(tenant2);
 		this.paymentMethods.add(paymentMethod1);
 		this.paymentMethods.add(paymentMethod2);
@@ -90,22 +105,36 @@ public class BookingTest {
 		when(this.stateAvailable.next()).thenReturn(stateApproved);
 		when(this.stateApproved.next()).thenReturn(stateCompleted);
 		when(this.stateCompleted.next()).thenReturn(stateAvailable);
+		when(this.pricer.price(this.checkIn)).thenReturn(pricePerDayWeekday);
+		when(this.pricer.priceBetween(this.checkIn, this.checkOut)).thenReturn(pricePerDayWeekday+pricePerDayHighSeason+pricePerDayLongWeekend);
 		
 		// SUT (System Under Test): objeto a testear
 		this.booking = new Booking(   stateAvailable
+									, pricer
 									, property
 									, checkIn
 									, checkOut
 									, paymentMethods
 									, pricePerDayWeekday
-									, pricePerDayHighSeason
-									, pricePerDayLongWeekend );
+									, periods );
+
+//		this.bookingReal = new Booking(   property
+//										, checkIn
+//										, checkOut
+//										, paymentMethods
+//										, pricePerDayWeekday
+//										, periods );
 	}
 
 	@Test
 	public void testConstructor() {
 		assertNotNull(this.booking);
 	}
+
+//	@Test
+//	public void testConstructorReal() {
+//		assertNotNull(this.bookingReal);
+//	}
 
 	@Test
 	public void testSetState() {
@@ -145,13 +174,32 @@ public class BookingTest {
 
 	@Test
 	public void testPrice() {
+		assertEquals(this.pricePerDayWeekday, this.booking.price(this.checkIn));
 	}
 	
 	@Test
 	public void testPriceBetween() {
+		double expectedPrice = this.pricePerDayWeekday + this.pricePerDayHighSeason + this.pricePerDayLongWeekend; 
+		assertEquals(expectedPrice, this.booking.priceBetween(this.checkIn, this.checkOut));
 	}
 	
 	@Test
-	public void testCancelBooking() {
+	public void testCancelReserve() {
+		ReserveAvailable spyState = mock(ReserveAvailable.class);
+		this.booking.setState(spyState);
+		assertEquals(spyState, this.booking.getState());
+		verify(spyState, times(0)).cancelReserve();
+		this.booking.cancelReserve();
+		verify(spyState, times(1)).cancelReserve();
+	}
+
+	@Test
+	public void testReserve() {
+		ReserveAvailable spyState = mock(ReserveAvailable.class);
+		this.booking.setState(spyState);
+		assertEquals(spyState, this.booking.getState());
+		verify(spyState, times(0)).requestReserve(this.tenant1);
+		this.booking.reserve(this.tenant1);
+		verify(spyState, times(1)).requestReserve(this.tenant1);
 	}
 }

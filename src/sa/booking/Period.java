50b987c6bfe5c76e544874ddf58cbef19457ad51
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-08 23:19:18 -0300

    Added IReserveState.java, ReserveAvailable.java, ReserveApproved.java, ReserveCompleted.java, Pricer.java and Period.java

---
+++ src/sa/booking/Period.java
@@ -0,0 +1,17 @@
+package sa.booking;
+
+import java.time.LocalDate;
+
+public class Period {
+
+	public boolean belongs(LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double price() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

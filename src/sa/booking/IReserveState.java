6ecd2fa897369741cebccecab611c720e7f088ff
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-10 16:05:51 -0300

    Fixed IReserveState.java

--- src/sa/booking/IReserveState.java
+++ src/sa/booking/IReserveState.java
@@ -1,12 +1,11 @@
 package sa.booking;
 
-import sa.users.Tenant;
 
 public interface IReserveState {
 
-	IReserveState next();
-	public void approbeReserve(Booking b);
-	public void cancelReserve();
-	public void requestReserve(Tenant t);
+	public IReserveState next();
+	public void approveReserve(Booking b);
+	public void cancelReserve(Booking b);
+	public void requestReserve(Booking b);
 
 }

b20861ac3efb85b69465184dae9e01dcc1a0595e
Ivan <elreizerwtf@gmail.com>
2024-11-11 15:44:53 -0300

    Actualizacion, ahora el metodo activate usa el metodo priceBetween del booking

--- src/sa/cancellation/NoCancellation.java
+++ src/sa/cancellation/NoCancellation.java
@@ -9,8 +9,9 @@ public class NoCancellation implements ICancellationPolicy{
 	@Override
 	public void activate(LocalDate date,Booking booking) {
 		// TODO Auto-generated method stub
-		System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.price(booking.getCheckIn())+" Pesos");
-		
+		//System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.price(booking.getCheckIn())+" Pesos");
+		System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.priceBetween(booking.getCheckIn(),booking.getCheckOut())+" Pesos");
+
 	}
 
 	

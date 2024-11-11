f2bdb25df8ac4ab7561315c384ba72cf5aba5dd6
Ivan <elreizerwtf@gmail.com>
2024-11-11 15:41:41 -0300

    Actualizacion metodo activate

--- src/sa/cancellation/NoCancellation.java
+++ src/sa/cancellation/NoCancellation.java
@@ -1,11 +1,13 @@
 package sa.cancellation;
 
+import java.time.LocalDate;
+
 import sa.booking.Booking;
 
 public class NoCancellation implements ICancellationPolicy{
 
 	@Override
-	public void activate(Booking booking) {
+	public void activate(LocalDate date,Booking booking) {
 		// TODO Auto-generated method stub
 		System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.price(booking.getCheckIn())+" Pesos");
 		

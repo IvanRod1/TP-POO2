5a94c0b7c2e8f8645c23b397eabef6073aa56933
Ivan <elreizerwtf@gmail.com>
2024-11-11 14:49:11 -0300

    Agrego comentario explicativo

--- src/sa/cancellation/CostFree.java
+++ src/sa/cancellation/CostFree.java
@@ -9,7 +9,13 @@ public class CostFree implements ICancellationPolicy{
 	private LocalDate date;
 	@Override
 	public void activate(Booking booking) {
-		// TODO Auto-generated method stub
+		/*
+		 * Este metodo se encarga de verificar si en el momento de activacion de la cancelacion 		   
+		 * fue 10 dias antes que el inicio del alquiler. En el caso de que sea asi, se notifica 
+		 * por pantalla que no se paga nada. En el caso negativo, tendra que pagar el equivalente
+		 * a dos dias de alquiler, se notificara por pantalla el valor
+		 * 
+		 * */
 		if(this.date.isBefore(booking.getCheckIn().minusDays(10))) {
 			System.out.println("No pagas nada");
 		}

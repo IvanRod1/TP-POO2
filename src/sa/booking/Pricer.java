540b7d7846fc2eb834c6de4f86722e18b130f66d
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-10 21:31:50 -0300

    Added PricerTest.java and Pricer.java

--- src/sa/booking/Pricer.java
+++ src/sa/booking/Pricer.java
@@ -1,21 +1,24 @@
 package sa.booking;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class Pricer {
+	
 	private double basePrice;
-	private List<Period> specialPeriods;
+	private List<SpecialPeriod> specialPeriods;
 
-	public Pricer(double pricePerDayWeekday, List<Period> periods) {
+	public Pricer(double pricePerDayWeekday, List<SpecialPeriod> periods) {
 		// TODO Auto-generated constructor stub
 		this.basePrice = pricePerDayWeekday;
 		this.specialPeriods = periods;
 	}
 
-	public void addSpecialPeriod(Period p) {
+	public void addSpecialPeriod(SpecialPeriod p) {
 		// TODO Auto-generated method stub
 		this.specialPeriods.add(p);
 	}
@@ -25,17 +28,12 @@ public class Pricer {
 		this.basePrice = pricePerDayWeekday;
 	}
 
-	public double price(LocalDate date) throws NoSuchElementException {
+	public double price(LocalDate date) {
 		// TODO Auto-generated method stub
-		try {
-			return this.specialPeriods.stream()
-				.filter(p -> p.belongs(date))
-				.findFirst()
-				.get()
-				.price();
-		} catch (NoSuchElementException e) {
-			return this.basePrice;
-		}
+		Optional<SpecialPeriod> sp = this.specialPeriods.stream()
+									.filter(p -> p.belongs(date))
+									.findFirst();
+		return sp.isPresent() ? sp.get().price() : this.basePrice;
 	}
 
 	public double priceBetween(LocalDate startDate, LocalDate endDate) {
@@ -47,6 +45,11 @@ public class Pricer {
 			currentDate = currentDate.plusDays(1);
 		}
 		return totalPrice + this.price(currentDate);		
+	}
+
+	public void removeSpecialPeriod(SpecialPeriod p) {
+		// TODO Auto-generated method stub
+		this.specialPeriods.remove(p);
 	}
 
 }

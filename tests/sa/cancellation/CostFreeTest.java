07de4889d416e1372ac073410579c827e1d3ac41
Ivan <elreizerwtf@gmail.com>
2024-11-14 10:15:48 -0300

    Actualizacion de tests

--- tests/sa/cancellation/CostFreeTest.java
+++ tests/sa/cancellation/CostFreeTest.java
@@ -12,19 +12,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import sa.booking.BookedPeriod;
 import sa.booking.Booking;
 
 class CostFreeTest {
 
 	private CostFree costfree;
 	private Booking bookingTest;
+	private BookedPeriod bookedPeriodMock;
 	@BeforeEach
 	void setUp() throws Exception {
 		costfree = new CostFree();
 		bookingTest = mock(Booking.class);
+		bookedPeriodMock = mock(BookedPeriod.class);
 		
-		when(bookingTest.getCheckIn()).thenReturn(LocalDate.of(2024, 11, 15));
+		when(bookedPeriodMock.getCheckIn()).thenReturn(LocalDate.of(2024, 11, 15));
+		//when(bookingTest.getCheckIn()).thenReturn(LocalDate.of(2024, 11, 15));
 		when(bookingTest.price(bookingTest.getCheckIn())).thenReturn((double) 9000);
+		
 	}
 
 	@Test
@@ -36,15 +41,15 @@ class CostFreeTest {
 	@Test
 	void activateCostFreeTest() {
 		CostFree spy = Mockito.spy(costfree);
-		spy.activate(LocalDate.of(2024, 11, 3),bookingTest);
-		verify(spy, atLeastOnce()).activate(LocalDate.of(2024, 11, 3), bookingTest);
+		spy.activate(LocalDate.of(2024, 11, 3),bookingTest,bookedPeriodMock);
+		verify(spy, atLeastOnce()).activate(LocalDate.of(2024, 11, 3), bookingTest,bookedPeriodMock);
 	}
 	
 	@Test
 	void activateNoCostFreeTest() {
 		CostFree spy = Mockito.spy(costfree);
-		spy.activate(LocalDate.of(2024, 11, 9),bookingTest);
-		verify(spy, atLeastOnce()).activate(LocalDate.of(2024, 11, 9), bookingTest);
+		spy.activate(LocalDate.of(2024, 11, 9),bookingTest,bookedPeriodMock);
+		verify(spy, atLeastOnce()).activate(LocalDate.of(2024, 11, 9), bookingTest,bookedPeriodMock);
 	}
 	
 

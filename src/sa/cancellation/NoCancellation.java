c0202f5b90931750834646a8754255cad5d62ec6
Ivan <elreizerwtf@gmail.com>
2024-11-14 10:06:52 -0300

    Actualizacion metodo activate

--- src/sa/cancellation/NoCancellation.java
+++ src/sa/cancellation/NoCancellation.java
@@ -2,20 +2,23 @@ package sa.cancellation;
 
 import java.time.LocalDate;
 
+import sa.booking.BookedPeriod;
 import sa.booking.Booking;
 
 public class NoCancellation implements ICancellationPolicy{
 
 	@Override
-	public void activate(LocalDate date,Booking booking) {
-
+	public void activate(LocalDate cancellationDate, Booking booking, BookedPeriod periodCancelled) {
 		/*
 		 * El metodo activate indica por pantalla la cantidad de dinero que tiene que pagar el inquilino
 		 * */
-		//System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.price(booking.getCheckIn())+" Pesos");
-		System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.priceBetween(booking.getCheckIn(),booking.getCheckOut())+" Pesos");
-
+		System.out.println("Se ha procesado la cancelación, tiene que abonar $"+booking.priceBetween(periodCancelled.getCheckIn(),periodCancelled.getCheckOut())+" Pesos");
+		
 	}
+
+	
+	
+	
 
 	
 }

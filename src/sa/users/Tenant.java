f326997fe8de99a2fb190054b0130a1a826db381
Ivan <elreizerwtf@gmail.com>
2024-11-16 16:03:18 -0300

    Clase tenant y tenant test

--- src/sa/users/Tenant.java
+++ src/sa/users/Tenant.java
@@ -1,10 +1,22 @@
 package sa.users;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import sa.accomodationsite.AccomodationSite;
+import sa.booking.Booking;
+import sa.paymentMethod.PaymentMethod;
+import sa.propertyRent.PropertyRent;
+
+
 public class Tenant extends User {
 	
-	public Tenant(String fullName, int telephone, String mail) {
+	public Tenant(String fullName, int telephone, String mail,AccomodationSite site) {
 		
-		super(fullName, telephone, mail);
+		super(fullName, telephone, mail,site);
 	}
 
 	@Override
@@ -20,5 +32,48 @@ public class Tenant extends User {
 	    System.out.println("Area: " + this.mail);
 		
 	}
+
+	public void makeReserve(PropertyRent aviso, LocalDate checkIn, LocalDate checkOut, PaymentMethod method) {
+		// TODO Auto-generated method stub
+		if(this.registeredSite.verifyBookingAttempt(aviso,checkIn,checkOut,method)) {
+			this.registeredSite.addBooking(checkIn,checkOut,this,aviso);
+		}
+		else {
+			System.out.println("No se pudo hacer la reserva, No esta disponible");
+		}
+		
+	}
+
+	public void cancelReserve(PropertyRent rent) {
+		this.registeredSite.getPropertyRent(rent).cancelReserve(this);
+		
+	}
+
+	public List<Booking> futureBookings() {
+		List<Booking> bookingsTenant = this.allBookings();
+		List<Booking> futureBookings = new ArrayList<Booking>();
+		
+		for(int i=0;i<bookingsTenant.size();i++) {
+			if(bookingsTenant.get(i).getCheckIn().isAfter(this.registeredSite.getCurrentDate())) {
+				futureBookings.add(bookingsTenant.get(i));
+			}
+		}
+		return futureBookings;
+	}
+
+	public List<Booking> allBookings() {
+		// TODO Auto-generated method stub
+		return this.registeredSite.getBookingsOf(this);
+	}
+
+	public Set<String> allBookingsCities() {
+		List<Booking> bookingsTenant = this.allBookings();
+		Set<String> cities = new HashSet<String>();
+		for(int i=0;i<bookingsTenant.size();i++) {
+			cities.add(bookingsTenant.get(i).getProperty().getCity());
+		}
+		return cities;
+	}
+	
 
 }
---
+++ tests/sa/users/TenantTest.java
@@ -0,0 +1,83 @@
+package sa.users;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sa.accomodationsite.AccomodationSite;
+import sa.booking.Booking;
+import sa.paymentMethod.PaymentMethod;
+import sa.propertyRent.PropertyRent;
+
+class TenantTest {
+
+	private Tenant userTenant;
+	private Tenant spyTenant;
+	private AccomodationSite site;
+	
+	private PropertyRent rent;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		site = mock(AccomodationSite.class);
+		userTenant = new Tenant("corcho",22,"correo",site);
+		spyTenant = spy(userTenant);
+		
+		rent = mock(PropertyRent.class);
+		
+	}
+
+	@Test
+	void newTenanTest() {
+		 //userTenant = new Tenant("Miu",22,"correo");
+		 assertNotNull(userTenant);
+	}
+	@Test
+	void tenantSummaryTest() {
+		spyTenant.summary();
+		verify(spyTenant).summary();
+	}
+	@Test
+	void getRankTenanTest() {
+		when(spyTenant.getRank()).thenReturn(3.2);
+		verify(spyTenant).getRank();
+	}
+	@Test
+	void makeReserveTest() {
+		PropertyRent aviso = mock(PropertyRent.class);
+		PaymentMethod cash = mock(PaymentMethod.class);
+		userTenant.makeReserve(aviso,LocalDate.of(2024,1, 10), LocalDate.of(2024,1,17),cash);
+	}
+	@Test
+	void cancelReserveTest() {
+		PropertyRent rentMock = mock(PropertyRent.class);
+		when(userTenant.registeredSite.getPropertyRent(rent)).thenReturn(rent);
+		userTenant.cancelReserve(rentMock);
+	}
+	@Test
+	void allBookingsTest() {
+		
+		when(userTenant.registeredSite.getBookingsOf(userTenant)).thenReturn(new ArrayList<Booking>());
+		assertEquals(userTenant.allBookings().size(),0);
+	}
+	@Test
+	void futureBookingsTest() {
+		when(userTenant.registeredSite.getBookingsOf(userTenant)).thenReturn(new ArrayList<Booking>());
+		assertEquals(userTenant.futureBookings().size(), 0);
+	}
+	@Test
+	void allBookingCitiesTest() {
+		when(userTenant.registeredSite.getBookingsOf(userTenant)).thenReturn(new ArrayList<Booking>());
+		assertEquals(userTenant.allBookingsCities().size(),0);
+	}
+
+}

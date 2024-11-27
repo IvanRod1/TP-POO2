0f143b963e2b59d7ef686ca4875652bc9254091b
Ivan <elreizerwtf@gmail.com>
2024-11-27 15:44:46 -0300

    MaxPrice y MaxPriceTest agregados

---
+++ src/sa/searcher/simpleQuery/MaxPrice.java
@@ -0,0 +1,32 @@
+package sa.searcher.simpleQuery;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import sa.booking.Booking;
+
+public class MaxPrice extends SimpleQuery {
+
+	private double value;
+	private LocalDate date;
+	
+	public MaxPrice(double value, LocalDate d) {
+		this.date = d;
+		this.value = value;
+	}
+	
+	@Override
+	public List<Booking> search(List<Booking> bookings) {
+		List<Booking> aux = new ArrayList<Booking>();
+		
+		for(int i=0;i<bookings.size();i++) {
+			if(bookings.get(i).price(date) <= this.value) {
+				aux.add(bookings.get(i));
+			}
+		}
+		
+		return aux;
+	}
+
+}

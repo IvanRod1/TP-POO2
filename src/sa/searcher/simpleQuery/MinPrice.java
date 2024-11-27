6e51e756645192a2668eff9be07c5e073ed1e13f
Ivan <elreizerwtf@gmail.com>
2024-11-27 15:12:23 -0300

    MinPrice y MinPriceTest agregados

---
+++ src/sa/searcher/simpleQuery/MinPrice.java
@@ -0,0 +1,32 @@
+package sa.searcher.simpleQuery;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import sa.booking.Booking;
+
+public class MinPrice extends SimpleQuery {
+
+	private double value;
+	private LocalDate date;
+	
+	public MinPrice(double min, LocalDate d) {
+		this.value = min;
+		this.date = d;
+	}
+
+	@Override
+	public List<Booking> search(List<Booking> bookings) {
+		List<Booking> aux = new ArrayList<Booking>();
+		
+		for(int i=0;i<bookings.size();i++) {
+			if(bookings.get(i).price(date) >= this.value ) {
+				aux.add(bookings.get(i));
+			}
+		}
+		
+		return aux;
+	}
+
+}

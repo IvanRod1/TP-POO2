8b5e1517bb61120699a42c405060fd66ff28ef78
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-26 23:28:49 -0300

    Added Timer

---
+++ src/sa/booking/Timer.java
@@ -0,0 +1,22 @@
+package sa.booking;
+
+import java.time.LocalDate;
+
+import sa.booking.reserveStates.INotifyTimer;
+
+public class Timer implements INotifyTimer {
+
+	@Override
+	public void register(Booking booking, Reserve reserve, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void unregister(Booking booking, Reserve r, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
---
+++ src/sa/booking/reserveStates/Timer.java
@@ -0,0 +1,12 @@
+package sa.booking.reserveStates;
+
+import java.time.LocalDate;
+
+public class Timer implements INotifyTimer {
+
+	public void register(IReserveState r, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

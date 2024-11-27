ac6d33dd99005b41e5e0084da3e4c4d4e17c9091
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-27 00:35:14 -0300

    Fixed protocol on Timer

--- src/sa/booking/reserveStates/Timer.java
+++ src/sa/booking/reserveStates/Timer.java
@@ -2,9 +2,18 @@ package sa.booking.reserveStates;
 
 import java.time.LocalDate;
 
+import sa.booking.Booking;
+import sa.booking.Reserve;
+
 public class Timer implements INotifyTimer {
 
 	public void register(IReserveState r, LocalDate date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void register(Booking booking, Reserve reserve, LocalDate date) {
 		// TODO Auto-generated method stub
 		
 	}

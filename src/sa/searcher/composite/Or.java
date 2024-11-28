364165176ef069a1f5eefec3ac4543fef5fb83b4
Ivan <elreizerwtf@gmail.com>
2024-11-28 15:42:47 -0300

    Agrego Or y OrTest

---
+++ src/sa/searcher/composite/Or.java
@@ -0,0 +1,32 @@
+package sa.searcher.composite;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import sa.booking.Booking;
+import sa.searcher.simpleQuery.IQuery;
+
+public class Or extends CompositeQuery {
+
+	private IQuery fstQuery;
+	private IQuery sndQuery;
+	
+	public Or(IQuery firstQuery, IQuery secondQuery) {
+		this.fstQuery = firstQuery;
+		this.sndQuery = secondQuery;
+	}
+	@Override
+	public List<Booking> search(List<Booking> bookings) {
+		Set<Booking> totalBookings = new LinkedHashSet<Booking>();
+		
+		totalBookings.addAll(fstQuery.search(bookings));
+		totalBookings.addAll(sndQuery.search(bookings));
+		
+		return new ArrayList<Booking>(totalBookings);
+	}
+
+}

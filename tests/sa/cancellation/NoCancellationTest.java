36e833ba48dfd923b09ef66158371adb13d5f157
Ivan <elreizerwtf@gmail.com>
2024-11-28 20:25:39 -0300

    actualizacion

--- tests/sa/cancellation/NoCancellationTest.java
+++ tests/sa/cancellation/NoCancellationTest.java
@@ -5,17 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import sa.booking.Reserve;
+
 class NoCancellationTest {
 
 	private NoCancellation cancellationtest;
+	private Reserve reserveMock;
+	
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		cancellationtest = new NoCancellation();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void newCancellationTest() {
+		assertNotNull(cancellationtest);
+	}
+	
+	@Test
+	void activateCancellationTest() {
+		cancellationtest.activate(reserveMock);
 	}
 
 }

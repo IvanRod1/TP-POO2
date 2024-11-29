1571a1bb6f1e12fdeb46adc491a071aa063ae4d6
Ivan <elreizerwtf@gmail.com>
2024-11-29 08:23:19 -0300

    Saco paquete innecesario

--- tests/sa/cancellation/IntermediateCancellationTest.java
+++ tests/sa/cancellation/IntermediateCancellationTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;

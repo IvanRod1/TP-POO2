16f65219da2c2c90eb0db5a1ecd35b6f8fc61efd
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-11-28 23:27:59 -0300

    Added ReserveBookedTest.java

--- src/sa/booking/reserveStates/ReserveBooked.java
+++ src/sa/booking/reserveStates/ReserveBooked.java
@@ -1,30 +1,29 @@
 package sa.booking.reserveStates;
 
+
 import java.time.LocalDate;
 
-import sa.booking.Booking;
 import sa.booking.Reserve;
-import sa.subscriptions.INotifyTimerSubscriber;
-import sa.users.Owner;
-import sa.users.Tenant;
 
 public class ReserveBooked implements IReserveState {
 
 	private IReserveState	nextState;
+	private IReserveState	cancelState;
 	private Reserve			reserve;
 	
 
 	public ReserveBooked(Reserve reserve) {
 		// TODO Auto-generated constructor stub
-		this.nextState 	  = new ReserveOccupied(reserve);
-		this.reserve  = reserve;
+		this.reserve  	  = reserve;
+		this.nextState 	  = new ReserveOccupied(this.reserve);
 	}
 
 	// Para hacer DOC del state approved
-	public ReserveBooked(ReserveOccupied stateNext, Reserve reserve) {
+	public ReserveBooked(ReserveOccupied stateNext, ReserveCancelled stateCancel, Reserve reserve) {
 		// TODO Auto-generated constructor stub
-		this.nextState 	  = stateNext;
-		this.reserve = reserve;
+		this.nextState 	 = stateNext;
+		this.cancelState = stateCancel;
+		this.reserve 	 = reserve;
 	}
 
 
@@ -33,25 +32,15 @@ public class ReserveBooked implements IReserveState {
 		// TODO Auto-generated method stub
 		this.getReserve().setState(this.nextState);
 	}
-
-	@Override
-	public void request(Reserve r) {
-		b.setState(this);
-		b.getProperty().getOwner().reserveRequestedOn(b, bp); // Se le avisa al propietario que decida para el bookedperiod actual
-	}
-
+	
 	@Override
 	public void cancel() {
 		// TODO Auto-generated method stub
 		// La cancelación es asentada en el sistema y se envía un mail al dueño del inmueble informándole sobre
 		// el hecho. Debe ser posible enviar por email alguna de las reservas realizadas.
-		this.getReserve().setState(new ReserveCancelled(this.getReserve()));
+		this.getReserve().setState(new ReserveCancelled(this.getReserve(), LocalDate.now()));
+		// el sistema (que ES un observer) debe registrar la cancelación y puede avisarle al propietario y al Tenant
 		this.getReserve().getBooking().notifySubscribersCancelled(this.getReserve());
-		bp.tenant().reserveCancelled(b, bp); // TODO borrar porque lo hace el sistema
-		b.getProperty().getOwner().reserveCancelled(b, bp); // TODO borrar porque lo hace el sistema
-		b.notifySubscribersCancelled(b, bp);  // el sistema (que ES un observer) debe registrar la cancelación y puede avisarle al propietario y al Tenant
-		b.applyPolicy(bp);
-		b.triggerNextRequest();
 	}
 
 	@Override
@@ -71,6 +60,7 @@ public class ReserveBooked implements IReserveState {
 	@Override
 	public Reserve getReserve() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.reserve;
 	}
+
 }
--- tests/sa/booking/reserveStates/ReserveBookedTest.java
+++ tests/sa/booking/reserveStates/ReserveBookedTest.java
@@ -5,20 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import static org.mockito.Mockito.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import sa.booking.BookedPeriod;
 import sa.booking.Booking;
-import sa.booking.reserveStates.ReserveAvailable;
-import sa.booking.reserveStates.ReserveBooked;
-import sa.booking.reserveStates.ReserveOccupied;
+import sa.booking.Reserve;
 import sa.properties.Property;
 import sa.users.Owner;
 import sa.users.Tenant;
@@ -26,9 +16,10 @@ import sa.users.Tenant;
 class ReserveBookedTest {
 
 	private ReserveBooked		stateBooked;
-	private ReserveAvailable	stateAvailable;
+	private ReserveCancelled	stateCancelled;
 	private ReserveOccupied		stateOccupied;
 	private Booking				booking;
+	private Reserve				reserve;
 	private Property			property;
 	private Tenant				tenant;
 	private Owner				owner;
@@ -36,20 +27,22 @@ class ReserveBookedTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		// DOC (Depended-On-Component): nuestros doubles
-		this.stateAvailable	= mock(ReserveAvailable.class);
+		this.stateCancelled	= mock(ReserveCancelled.class);
 		this.stateOccupied	= mock(ReserveOccupied.class);
 		this.booking		= mock(Booking.class);
+		this.reserve		= mock(Reserve.class);
 		this.property		= mock(Property.class);
 		this.tenant			= mock(Tenant.class);
 		this.owner			= mock(Owner.class);
 
 		when(this.booking.getProperty()).thenReturn(this.property);
-		when(this.booking.getTenant()).thenReturn(this.tenant);
+		when(this.reserve.getTenant()).thenReturn(this.tenant);
+		when(this.reserve.getBooking()).thenReturn(this.booking);
 		when(this.property.getOwner()).thenReturn(this.owner);
 		
 		
 		// SUT (System Under Test): objeto a testear
-		this.stateBooked = new ReserveBooked(this.stateOccupied, this.stateAvailable);
+		this.stateBooked = new ReserveBooked(this.stateOccupied, this.stateCancelled, this.reserve);
 	}
 
 	@Test
@@ -59,59 +52,32 @@ class ReserveBookedTest {
 
 	@Test
 	void testNext() {
-		assertEquals(this.stateOccupied, this.stateBooked.next());
-	}
-	
-	@Test
-	void testRequestReserve() {
-		verify(this.booking, times(0)).setState(stateBooked);
-		this.stateBooked.requestReserve(this.booking, this.bp);
-		verify(this.booking, times(1)).setState(stateBooked);
-		
-//		
-//		verify(this.stateBooked, times(1)).requestReserve(this.booking, this.bp);
-//		verify(this.booking, times(1)).getProperty();
-//		verify(this.property, times(1)).getOwner();
-//		verify(this.owner, times(1)).reserveRequestedOn(this.booking, this.bp);
-//		// Simular aprobación
-//		verify(this.booking, times(0)).approveReserve(this.bp);
-//		this.booking.approveReserve(this.bp);
-//		verify(this.booking, times(1)).approveReserve(this.bp);
-//		// Simular rechazo
-//		verify(this.booking, times(0)).cancelReserve(this.bp);
-//		this.booking.cancelReserve(this.bp);
-//		verify(this.booking, times(1)).cancelReserve(this.bp);
-//
-//		this.stateBooked.requestReserve(this.booking);
-//		verifyNoInteractions(this.booking);
-//		verifyNoInteractions(this.property);
-//		verifyNoInteractions(this.owner);
+		verifyNoInteractions(this.reserve);
+		this.stateBooked.next();
+		verify(this.reserve, times(1)).setState(this.stateOccupied); 
 	}
 
 	@Test
-	void testApproveReserve() {
-		this.stateBooked.approveReserve(this.booking, this.bp);
-		verify(this.booking, times(1)).setTenant(this.tenant);
-		verify(this.booking, times(1)).notifySubscribersReserve(this.booking, this.bp);
-		verify(this.stateOccupied, times(1)).requestReserve(this.booking, this.bp);
+	void testApprove() {
+		this.stateBooked.approve(this.reserve);
+		verify(this.booking, times(1)).notifySubscribersReserve(this.reserve);
 	}
 
 	@Test
-	void testCancelReserve() {
-		this.stateBooked.cancelReserve(this.booking, this.bp);
-		verify(this.booking, times(1)).setState(stateAvailable);
-		verify(this.booking, times(1)).setTenant(null);
-		verify(this.tenant, times(1)).reserveCancelled(this.booking, this.bp);
-		verify(this.owner, times(1)).reserveCancelled(this.booking, this.bp);
-		verify(this.booking, times(1)).notifySubscribersCancelled(this.booking, this.bp);
-		verify(this.booking, times(1)).triggerNextRequest();
-	
-//		this.stateBooked.cancelReserve(this.booking, this.bp);
-//		verify(this.booking, times(1)).getTenant();
-//		verify(this.booking, times(1)).getProperty();
-//		verify(this.booking, times(1)).triggerNextRequest();
-//		verify(this.tenant, times(1)).reserveCancelled(this.booking, this.bp);
-//		verify(this.owner, times(1)).reserveCancelled(this.booking, this.bp);
+	void testCancel() {
+		verify(this.reserve, times(0)).setState(this.stateCancelled);
+		verify(this.booking, times(0)).notifySubscribersCancelled(this.reserve);
+		this.stateBooked.cancel();
+		verify(this.booking, times(1)).notifySubscribersCancelled(this.reserve);
 	}
 
+	@Test
+	void testGetReserve() {
+		assertEquals(this.reserve, this.stateBooked.getReserve());
+	}
+
+	@Test
+	void testIsCancelled() {
+		assertEquals(false, this.stateBooked.isCancelled());
+	}
 }

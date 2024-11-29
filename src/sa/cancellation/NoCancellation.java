2729f0278a3f3da9844dd77bc847ac2e8a4a1932
Ivan <elreizerwtf@gmail.com>
2024-11-29 07:27:50 -0300

    Actualizo NoCancellation y su test

--- src/sa/cancellation/NoCancellation.java
+++ src/sa/cancellation/NoCancellation.java
@@ -1,12 +1,14 @@
 package sa.cancellation;
 
 
+import java.time.LocalDate;
+
 import sa.booking.Reserve;
 
 public class NoCancellation implements ICancellationPolicy {
 
 	@Override
-	public void activate(Reserve reserve) {
+	public void activate(Reserve reserve,LocalDate cancellationDate) {
 		System.out.println("La cancelacion se efectuó, se debe pagar el monto de $: "+reserve.getPrice());
 		
 	}
--- tests/sa/cancellation/NoCancellationTest.java
+++ tests/sa/cancellation/NoCancellationTest.java
@@ -1,6 +1,10 @@
 package sa.cancellation;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +16,15 @@ class NoCancellationTest {
 	private NoCancellation cancellationtest;
 	private Reserve reserveMock;
 	
+	private LocalDate dayTest;
+	
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		cancellationtest = new NoCancellation();
+		dayTest = LocalDate.now();
+		
+		reserveMock = mock(Reserve.class);
 	}
 
 	@Test
@@ -25,7 +34,8 @@ class NoCancellationTest {
 	
 	@Test
 	void activateCancellationTest() {
-		cancellationtest.activate(reserveMock);
+		cancellationtest.activate(reserveMock,dayTest);
+		verify(reserveMock).getPrice();
 	}
 
 }

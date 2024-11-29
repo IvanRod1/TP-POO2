60014788cb79358bc42a648d4aafd3e43687e2f4
Ivan <elreizerwtf@gmail.com>
2024-11-29 07:28:05 -0300

    Cambio metodo de interfaz

--- src/sa/cancellation/ICancellationPolicy.java
+++ src/sa/cancellation/ICancellationPolicy.java
@@ -1,8 +1,11 @@
 package sa.cancellation;
 
+import java.time.LocalDate;
+
 import sa.booking.Reserve;
 
 public interface ICancellationPolicy {
 
-	public void activate(Reserve reserve);
+	//Se necesita el localdate para poder modificar el dia de cancelacion en los test
+	public void activate(Reserve reserve, LocalDate cancellationDate);
 }

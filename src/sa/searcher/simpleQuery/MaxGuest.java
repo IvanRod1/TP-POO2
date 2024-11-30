b54e7da2c6492e21603c4544841257b972e7a766
Ivan <elreizerwtf@gmail.com>
2024-11-30 12:43:34 -0300

    MaxGuest y MaxGuestTest actualizados

--- src/sa/searcher/simpleQuery/MaxGuest.java
+++ src/sa/searcher/simpleQuery/MaxGuest.java
@@ -1,34 +1,31 @@
 package sa.searcher.simpleQuery;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import sa.booking.Booking;
 
 public class MaxGuest extends SimpleQuery {
 
-	private int value;
+private int value;
+	
+	public MaxGuest(int quantity) {
+		this.value = quantity;
+	}
+
 	@Override
-	public ArrayList<Booking> search(ArrayList<Booking> bookings) {
-		ArrayList<Booking> aux = new ArrayList<Booking>();
+	public List<Booking> search(List<Booking> bookings) {
+		List<Booking> aux = new ArrayList<Booking>();
 		
 		for(int i=0;i<bookings.size();i++) {
-			if(bookings.get(i).getProperty().getMaxGuests() >= this.getMaxGuests()) {
+			if(this.value <= bookings.get(i).getProperty().getMaxGuests()) {
 				aux.add(bookings.get(i));
 			}
 		}
 		
 		return aux;
 	}
-	
-	public MaxGuest(int max) {
-		this.value = max;
-	}
-	
 
-	public void setMaxGuest(int i) {
-		this.value = i;
-	}
-	
 	public int getMaxGuests() {
 		return this.value;
 	}

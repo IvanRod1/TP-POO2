21990030574707c42ce02c86d388f045fe16388f
Ivan <elreizerwtf@gmail.com>
2024-11-30 12:39:42 -0300

    CheckOut y CheckOutTest actualizados

--- src/sa/searcher/simpleQuery/CheckOut.java
+++ src/sa/searcher/simpleQuery/CheckOut.java
@@ -2,34 +2,32 @@ package sa.searcher.simpleQuery;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import sa.booking.Booking;
 
 public class CheckOut extends SimpleQuery{
 
-	private LocalDate date;
+private LocalDate date;
 	
+	public CheckOut(LocalDate d) {
+		this.date = d;
+	}
+
 	@Override
-	public ArrayList<Booking> search(ArrayList<Booking> bookings) {
-		ArrayList<Booking> aux = new ArrayList<Booking>();
-		for(int i=0;i<bookings.size();i++)
-		{
-			//bookings.get(i).getCheckOut().isBefore(this.checkout)
-			if(this.getDate().isBefore(bookings.get(i).getCheckOut())|| bookings.get(i).getCheckOut().isEqual(this.getDate())) {
+	public List<Booking> search(List<Booking> bookings) {
+		List<Booking> aux = new ArrayList<Booking>();
+		for(int i=0;i<bookings.size();i++) {
+			if(bookings.get(i).getPeriod().belongs(getCheckOutDate())) {
 				aux.add(bookings.get(i));
 			}
 		}
 		return aux;
 	}
-	public LocalDate getDate() {
+
+	public LocalDate getCheckOutDate() {
+		// TODO Auto-generated method stub
 		return this.date;
-	}
-	
-	public CheckOut(LocalDate date) {
-		this.date = date;
-	}
-	public void setDate(LocalDate fecha) {
-		this.date = fecha;
 	}
 
 }

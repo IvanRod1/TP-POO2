6a2a4923ca3e99a1863d9c529f9ff99e4cffa575
Ivan <elreizerwtf@gmail.com>
2024-11-30 12:42:07 -0300

    IntermediateCancellation e IntermediateCancellationTest actualizados

--- src/sa/cancellation/IntermediateCancellation.java
+++ src/sa/cancellation/IntermediateCancellation.java
@@ -2,29 +2,21 @@ package sa.cancellation;
 
 import java.time.LocalDate;
 
-import sa.booking.BookedPeriod;
-import sa.booking.Booking;
+import sa.booking.Reserve;
 
 public class IntermediateCancellation implements ICancellationPolicy{
 
-	
-
 	@Override
-	public void activate(LocalDate cancellationDate, Booking booking, BookedPeriod periodCancelled) {
-		/*
-		 * Este metodo se encarga de verificar la fecha de cancelacion del booking con la fecha de activacion de la politica
-		 * Si la fecha de cancelacion es 20 dias previa, no se paga nada y se lo notifica en pantalla
-		 * Si es 10 dias previa, se paga la mitad del precio
-		 * Y si no es ninguna de las 2, se paga todo 
-		 * */
+	public void activate(Reserve reserve, LocalDate cancellationDate) {
 		
-		if(cancellationDate.plusDays(20).isBefore(periodCancelled.start())) {
-			System.out.println("No pagas nada");
-		}else if (cancellationDate.plusDays(10).isBefore(periodCancelled.start())) {
-			System.out.println("Se paga la mitad "+booking.priceBetween(periodCancelled.start(),periodCancelled.end())/2);
+		if(cancellationDate.isBefore(reserve.getCheckIn().minusDays(20))) {
+			System.out.println("La cancelación se asentó correctamente de manera gratuita");
+			reserve.setPrice(0.0);
+		}else if(cancellationDate.isBefore(reserve.getCheckIn().minusDays(19)) || cancellationDate.isBefore(reserve.getCheckIn().minusDays(10)) ) {
+			System.out.println("La cancelación se asentó correctamente, se paga la mitad de la reserva");
+			reserve.setPrice(reserve.getPrice() * 0.5);
 		}else {
-			System.out.println("Se paga todo "+booking.priceBetween(periodCancelled.start(),periodCancelled.end()));
-
+			System.out.println("La cancelación se asentó correctamente, se paga todo");
 		}
 		
 	}

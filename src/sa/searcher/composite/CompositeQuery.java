22c7d2020449af11e8b07916a9f936ea4242283c
Ivan <elreizerwtf@gmail.com>
2024-11-30 12:40:09 -0300

    actualizado

--- src/sa/searcher/composite/CompositeQuery.java
+++ src/sa/searcher/composite/CompositeQuery.java
@@ -1,12 +1,9 @@
 package sa.searcher.composite;
-import java.util.ArrayList;
 
-import sa.booking.Booking;
 import sa.searcher.simpleQuery.IQuery;
 
 
 public abstract class CompositeQuery implements IQuery{
 
-	@Override
-	public abstract ArrayList<Booking> search(ArrayList<Booking> bookings);
+
 }

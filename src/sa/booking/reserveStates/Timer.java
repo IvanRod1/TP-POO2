8ff24a68f89c14eac06285ec46f3e1de1be7be57
Martín González Buitrón <martingonzalezbuitron@gmail.com>
2024-12-03 18:33:59 -0300

    Minor change

--- src/sa/booking/reserveStates/Timer.java
+++ src/sa/booking/reserveStates/Timer.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ForkJoinPool;
 
 import sa.booking.Booking;
 import sa.booking.Reserve;

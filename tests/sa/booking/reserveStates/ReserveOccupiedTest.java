12b51009f8be4ed2cd030527087bb57c75475c1b
Ivan <elreizerwtf@gmail.com>
2024-12-06 11:11:07 -0300

    Actualizo ReserveOccupiedTest

--- tests/sa/booking/reserveStates/ReserveOccupiedTest.java
+++ tests/sa/booking/reserveStates/ReserveOccupiedTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 import sa.booking.Booking;
+import sa.booking.Period;
 import sa.booking.Reserve;
 import sa.properties.Property;
 import sa.users.Owner;
@@ -15,12 +16,16 @@ import sa.users.Tenant;
 
 class ReserveOccupiedTest {
 
-	private ReserveOccupied		stateOccupied;
+	private ReserveOccupied		stateOccupied1;
+	private ReserveOccupied		stateOccupied2;
 	private Booking				booking;
 	private Property			property;
 	private Tenant				tenant;
 	private Owner				owner;
 	private Reserve				reserve;
+	private Timer 				timer;
+	private Reserve				spyReserve;
+	private Period				period;
 	
 	
 	@BeforeEach
@@ -31,47 +36,61 @@ class ReserveOccupiedTest {
 		this.tenant			= mock(Tenant.class);
 		this.owner			= mock(Owner.class);
 		this.reserve		= mock(Reserve.class);
+		this.timer          = mock(Timer.class);
+		this.period			= mock(Period.class);
+		
+		this.spyReserve		= spy(new Reserve(booking,tenant,period));
 
 		when(this.booking.getProperty()).thenReturn(this.property);
 		when(this.reserve.getTenant()).thenReturn(this.tenant);
 		when(this.reserve.getBooking()).thenReturn(this.booking);
 		when(this.property.getOwner()).thenReturn(this.owner);
+		when(booking.getTimer()).thenReturn(timer);
 		
 		
 		// SUT (System Under Test): objeto a testear
-		this.stateOccupied = new ReserveOccupied(this.reserve);
+		this.stateOccupied1 = new ReserveOccupied(this.reserve);
+		this.stateOccupied2 = new ReserveOccupied(this.spyReserve);
 	}
 
 	@Test
 	void testConstructor() {
-		assertNotNull(this.stateOccupied);
+		assertNotNull(this.stateOccupied1);
+		assertNotNull(this.stateOccupied2);
 	}
 
-	@Test
-	void testNext() {
-		this.stateOccupied.next();
-	}
+//	@Test
+//	void testNext() {
+//		this.stateOccupied.next();		no existe mas este metodo
+//	}
 
-	@Test
-	void testApprove() {
-		this.stateOccupied.approve(this.reserve);
-	}
-
+//	@Test
+//	void testApprove() {
+//		this.stateOccupied.approve(this.reserve); no existe mas este metodo
+//	}
+//
 	@Test
 	void testCancel() {
-		verify(this.booking, times(0)).notifySubscribersCancelled(this.reserve);
-		this.stateOccupied.cancel();
-		verify(this.booking, times(1)).notifySubscribersCancelled(this.reserve);
+		verify(this.booking, times(0)).notifySubscribersCancelled(this.spyReserve);
+		this.stateOccupied2.cancel();
+		verify(this.booking, times(1)).handleCancellation(this.spyReserve);
 	}
-	
+//	
 	@Test
 	void testGetReserve() {
-		assertEquals(this.reserve, this.stateOccupied.getReserve());
+		assertEquals(this.reserve, this.stateOccupied1.getReserve());
 	}
 
 	@Test
 	void testIsCancelled() {
-		assertEquals(false, this.stateOccupied.isCancelled());
+		assertEquals(false, this.stateOccupied1.isCancelled());
+	}
+	
+	@Test
+	void testUpdate() {
+		this.stateOccupied2.update();
+		verify(this.booking.getTimer(),times(1)).unregister(this.stateOccupied2,this.stateOccupied2.getReserve().getCheckOut());
+		verify(this.stateOccupied2.getReserve(),times(1)).finished();
 	}
 
 }
